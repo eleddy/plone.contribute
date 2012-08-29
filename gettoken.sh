2955048c371ac2c90fc0f6309d1a30684f2a2818
Elizabeth Leddy <eleddy@eastofeaton.com>
2012-08-29 12:47:53 -0300

    small script to get a github token for use with github integration

---
+++ gettoken.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+echo "This will generate a new token for use with the github API."
+echo "You must have admin priveleges of the team you want to manipulate."
+read -p "Please enter your github username: "
+# Just goes to show, I have limited experience with awk
+curl -s -u $REPLY -d '{"scopes":["user"],"note":"Contributor Agreement Integration"}' \
+  https://api.github.com/authorizations | sed -e 's/^[ \n]*//' | awk -F=": " -v RS=", " ' /token/ { print $1}' | cut -c 11- | rev | cut -c 2- | rev
+
+
+
