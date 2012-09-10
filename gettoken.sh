07d7f30b64a545890ed7a117728e82c52f444fc3
Elizabeth Leddy <eleddy@eastofeaton.com>
2012-09-09 17:06:50 -0700

    actually add the needed permissions

--- gettoken.sh
+++ gettoken.sh
@@ -5,7 +5,7 @@ echo "This will generate a new token for use with the github API."
 echo "You must have admin priveleges of the team you want to manipulate."
 read -p "Please enter your github username: "
 # Just goes to show, I have limited experience with awk
-curl -s -u $REPLY -d '{"scopes":["user,public_repo,repo"],"note":"Contributor Agreement Integration"}' \
+curl -s -u $REPLY -d '{"scopes":["user","public_repo","repo"],"note":"Plone Contributor Agreement Integration"}' \
   https://api.github.com/authorizations | sed -e 's/^[ \n]*//' | awk -F=": " -v RS=", " ' /token/ { print $1}' | cut -c 11- | rev | cut -c 2- | rev
 
 
